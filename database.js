3f3844e543576fedd8e347276852b2add6c7bc03
unknown <alijahangir95@gmail.com>
2022-11-01 12:41:13 +0500

    local host removed

--- database.js
+++ database.js
@@ -6,7 +6,7 @@ const { Pool } = postgres;
 const pool = new Pool({
   user: "postgres",
   password: config.get("password"),
-  host: "localhost",
+  host: "127.0.0.1",
   port: 5432,
   database: "todo_pern_app",
 });
