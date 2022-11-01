352e6a81f4120c918e4efcc257510880f9d0a675
unknown <alijahangir95@gmail.com>
2022-11-01 13:25:45 +0500

    modified

--- database.js
+++ database.js
@@ -6,7 +6,7 @@ const { Pool } = postgres;
 const pool = new Pool({
   user: "postgres",
   password: config.get("password"),
-  host: "127.0.0.1",
+  host: "database:5432",
   database: "todo_pern_app",
 });
 
